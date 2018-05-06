b76d48fdb2c5d1dfb8b80160b8d7dfd3ce843a89
LizaVasyukova <liza1998v@yandex.ru>
2018-05-06 12:57:20 +0300

    Delete

--- Curs_RPP/ui_widget_add.h
+++
@@ -1,4 +0,0 @@
-#ifndef UI_WIDGET_ADD_H
-#define UI_WIDGET_ADD_H
-
-#endif // UI_WIDGET_ADD_H
--- Curs_RPP/widget_add.cpp
+++
@@ -1,9 +0,0 @@
-#include "widget_add.h"
-#include "ui_widget_add.h"
-
-Widget::Widget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Widget)
-{
-    ui->setupUi(this);
-}
